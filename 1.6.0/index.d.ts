c800a145e89c42e10b473bfb1db5ad8c5927ab35
Nathan Vecchiarelli <nathan.vecchiarelli@jefferson.edu>
2016-08-23 13:15:23 -0400

    removed unneeded namespace

--- 1.6.0/index.d.ts
+++ 1.6.0/index.d.ts
@@ -2,26 +2,22 @@
 // Project: https://github.com/floridoo/gulp-sourcemaps
 // Original Definitions by: Asana <https://asana.com>
 
-declare namespace sourcemaps {
-  interface InitOptions {
-    loadMaps?: boolean;
-    debug?: boolean;
-  }
-
-  interface WriteMapper {
-    (filePath: string): string;
-  }
-
-  interface WriteOptions {
-    addComment?: boolean;
-    includeContent?: boolean;
-    sourceRoot?: string | WriteMapper;
-    sourceMappingURLPrefix?: string | WriteMapper;
-  }
-
-  export function init(opts?: InitOptions): NodeJS.ReadWriteStream;
-  export function write(path?: string, opts?: WriteOptions): NodeJS.ReadWriteStream;
-  export function write(opts?: WriteOptions): NodeJS.ReadWriteStream;
+interface InitOptions {
+  loadMaps?: boolean;
+  debug?: boolean;
 }
 
-export = sourcemaps;
+interface WriteMapper {
+  (filePath: string): string;
+}
+
+interface WriteOptions {
+  addComment?: boolean;
+  includeContent?: boolean;
+  sourceRoot?: string | WriteMapper;
+  sourceMappingURLPrefix?: string | WriteMapper;
+}
+
+export function init(opts?: InitOptions): NodeJS.ReadWriteStream;
+export function write(path?: string, opts?: WriteOptions): NodeJS.ReadWriteStream;
+export function write(opts?: WriteOptions): NodeJS.ReadWriteStream;
--- 2.0.0-alpha/index.d.ts
+++ 2.0.0-alpha/index.d.ts
@@ -4,37 +4,34 @@
 
 import * as File from 'vinyl';
 
-declare namespace gulpsourcemaps {
-  interface InitOptions {
-    loadMaps?: boolean;
-    identityMap?: boolean;
-    debug?: boolean;
-  }
-
-  interface WriteMapper {
-      (file: string): string;
-  }
-
-  interface WriteFileMapper {
-      (file: File): string;
-  }
-
-  interface WriteOptions {
-      addComment?: boolean;
-      includeContent?: boolean;
-      sourceRoot?: string | WriteMapper;
-      destPath?: string;
-      sourceMappingURLPrefix?: string | WriteMapper;
-      sourceMappingURL?: WriteFileMapper;
-      mapFile?: WriteMapper;
-      mapSources?: WriteMapper;
-      debug?: boolean;
-      charset?: string;
-  }
-
-  export function init(opts?: InitOptions): NodeJS.ReadWriteStream;
-  export function write(path?: string, opts?: WriteOptions): NodeJS.ReadWriteStream;
-  export function write(opts?: WriteOptions): NodeJS.ReadWriteStream;
+interface InitOptions {
+loadMaps?: boolean;
+identityMap?: boolean;
+debug?: boolean;
 }
 
-export = gulpsourcemaps;
+interface WriteMapper {
+    (file: string): string;
+}
+
+interface WriteFileMapper {
+    (file: File): string;
+}
+
+interface WriteOptions {
+    addComment?: boolean;
+    includeContent?: boolean;
+    sourceRoot?: string | WriteMapper;
+    destPath?: string;
+    sourceMappingURLPrefix?: string | WriteMapper;
+    sourceMappingURL?: WriteFileMapper;
+    mapFile?: WriteMapper;
+    mapSources?: WriteMapper;
+    debug?: boolean;
+    charset?: string;
+}
+
+export function init(opts?: InitOptions): NodeJS.ReadWriteStream;
+export function write(path?: string, opts?: WriteOptions): NodeJS.ReadWriteStream;
+export function write(opts?: WriteOptions): NodeJS.ReadWriteStream;
+
