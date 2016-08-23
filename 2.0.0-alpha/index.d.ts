472e051cb40c1f8ab2a616d703c0179202c3e211
Nathan Vecchiarelli <nathan.vecchiarelli@jefferson.edu>
2016-08-23 12:34:39 -0400

    added 2.0.0-alpha type definitions

---
+++ 2.0.0-alpha/index.d.ts
@@ -0,0 +1,41 @@
+// Type definitions for gulp-sourcemaps v2.0.0-alpha
+// Project: https://github.com/floridoo/gulp-sourcemaps
+// Original Definitions by: Asana <https://asana.com>
+
+import * as File from "vinyl"
+
+declare namespace gulpsourcemaps {
+  interface InitOptions {
+    loadMaps?: boolean;
+    identityMap?: boolean;
+    debug?: boolean;
+  }
+
+  interface WriteMapper {
+      (file: string): string;
+  }
+
+  interface WriteFileMapper {
+      // file parameter needs to be updated with the vinyl file object definition
+      (file: any): string;
+  }
+
+  interface WriteOptions {
+      addComment?: boolean;
+      includeContent?: boolean;
+      sourceRoot?: string | WriteMapper;
+      destPath?: string;
+      sourceMappingURLPrefix?: string | WriteMapper;
+      sourceMappingURL?: WriteFileMapper;
+      mapFile?: WriteMapper;
+      mapSources?: WriteMapper;
+      debug?: boolean;
+      charset?: string;
+  }
+
+  export function init(opts?: InitOptions): NodeJS.ReadWriteStream;
+  export function write(path?: string, opts?: WriteOptions): NodeJS.ReadWriteStream;
+  export function write(opts?: WriteOptions): NodeJS.ReadWriteStream;
+}
+
+export = gulpsourcemaps;
