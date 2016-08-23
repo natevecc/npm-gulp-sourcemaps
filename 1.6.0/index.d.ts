1311eb1acea29a9e243995a122ccd24288b012b2
Nathan Vecchiarelli <nathan.vecchiarelli@jefferson.edu>
2016-08-23 12:25:54 -0400

    got 1.6.0 to build and test

---
+++ 1.6.0/index.d.ts
@@ -0,0 +1,27 @@
+// Type definitions for gulp-sourcemaps v2.0.0-alpha
+// Project: https://github.com/floridoo/gulp-sourcemaps
+// Original Definitions by: Asana <https://asana.com>
+
+declare module sourcemaps {
+  interface InitOptions {
+    loadMaps?: boolean;
+    debug?: boolean;
+  }
+
+  interface WriteMapper {
+    (filePath: string): string;
+  }
+
+  interface WriteOptions {
+    addComment?: boolean;
+    includeContent?: boolean;
+    sourceRoot?: string | WriteMapper;
+    sourceMappingURLPrefix?: string | WriteMapper;
+  }
+
+  export function init(opts?: InitOptions): NodeJS.ReadWriteStream;
+  export function write(path?: string, opts?: WriteOptions): NodeJS.ReadWriteStream;
+  export function write(opts?: WriteOptions): NodeJS.ReadWriteStream;
+}
+
+export = sourcemaps;
